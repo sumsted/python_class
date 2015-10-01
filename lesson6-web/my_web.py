9796de828214078b18d457603d76016c4695c7d9
Scott Umsted <scottumsted@gmail.com>
2015-10-01 06:48:59 -0500

    completed lesson 6

--- lesson6-web/my_web.py
+++ lesson6-web/my_web.py
@@ -3,20 +3,27 @@ from bottle import route, run, template, static_file, get, post, request
 
 @get('/')
 def get_index():
-    return template('index')
+    return template('index',result='')
+
+
 
 
 @post('/multiply')
 def post_index():
     # get values from request and store them in variables
-    first_number = int(request.forms.get('first_number'))
-    second_number = int(request.forms.get('second_number'))
+    first_number = int(request.forms.get('first-number'))
+    second_number = int(request.forms.get('second-number'))
 
     # calculate and store product in variable
     product = first_number * second_number
 
-    # pass product to index, assign result to result parameter
-    return template('index', result=product)
+    # format result
+    result = '%d x %d = %d' % (first_number, second_number, product)
+
+    # display form with result
+    return template('index', result=result)
+
+
 
 
 @route('/static/<filepath:path>')
@@ -27,4 +34,4 @@ def server_static(filepath):
     return static_file(filepath, root='./static')
 
 
-run(host='localhost', port=8080, reloader=True)
+run(host='localhost', port=8080)#, reloader=True)
