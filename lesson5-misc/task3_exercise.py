80678affd990dc2ad0f41bd89d09447b69ba17c3
Scott Umsted <scottumsted@gmail.com>
2015-10-04 07:47:02 -0500

    added solved exercise to lesson 5 and notes to lesson 6

--- lesson5-misc/task3_exercise.py
+++ lesson5-misc/task3_exercise.py
@@ -4,15 +4,12 @@ import animals
 
 # ask the user for their favorite animal and store it in a variable called favorite
 
-
+favorite = input('What is your favorite animal? ')
 
 # using an if statement check to see if favorite is in the animals_dict
-
-
-# if favorite is found print favorite and the value or definition of it
-
-
-# if favorite is not found print 'sorry, we can't find favorite'
-
-
-
+if favorite in animals.animals_dict:
+    # if favorite is found print favorite and the value or definition of it
+    print('%s - %s' % (favorite, animals.animals_dict[favorite]))
+else:
+    # if favorite is not found print 'sorry, we can't find favorite'
+    print('sorry, we can\'t find favorite')
