20a5078e123b0d6f19b83c056b1072e819b22610
Scott Umsted <scottumsted@gmail.com>
2015-10-16 23:13:29 -0500

    add exercise

---
+++ lesson8-study/exercise.py
@@ -0,0 +1,76 @@
+# 1. what is the output of the following code
+
+x = 3
+y = 8
+
+if x >= 0 and y < 9:
+    print('yes')
+else:
+    print('no')
+
+# 2. what is the output of the following code
+x = -3
+y = 8
+
+if x > 0 or y < 9:
+    print('yes')
+else:
+    print('no')
+
+# 3. convert the following equation to Python,
+#    replace None with your answer
+#            2
+#       (2+x)
+#  y = -------
+#         9
+
+x = 4
+y = None
+print(y)
+
+
+# 4. write code that converts Fahrenheit to Celsius
+#    1. ask the user to enter a temperature in Fahrenheit, store in variable f
+#    2. convert the variable f to an integer
+#    3. subtract 32 from f and divide the result by 1.8, store in variable c
+#    4. print out the temperature in variable c
+
+
+
+# 5. write code that loops from 0 to 19 using range()
+#    for each iteration raise the current loop value to the power of 3
+#    print the result of each iteration
+
+
+
+# 6. write a function called add_ten
+#    the function should take a parameter called
+#    number and return the number with 10 added to it
+
+
+
+# 7. create a list called my_friends
+#    add your friends names to it
+#    then loop through the list to print each name
+
+
+# 8. add the following cities and populations to the dictionary
+#    then check to see if 'Dallas' is in the dictionary
+#    if Dallas is print the population
+#    if Dallas is not in the dictionary print 'not found'
+#    New York    8,491,079
+#    Los Angeles 3,928,864
+#    Chicago	 2,722,389
+#    Houston	 2,239,558
+
+city_dict = {}
+
+if None:
+    print(city_dict['Dallas'])
+else:
+    print('not found')
+
+# 9. import the add_message from the echo module
+#    call the add_message function pass your name and a message
+#    as parameters and print the result
+
